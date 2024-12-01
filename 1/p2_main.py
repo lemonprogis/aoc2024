5ed5ce5004c8d585eba5620744873c24775da058
lemonprogis <edward.briggler@gmail.com>
2024-12-01 15:03:30 -0600

    part deux

---
+++ 1/p2_main.py
@@ -0,0 +1,26 @@
+
+
+def do_the_thing(l1, l2):
+    d = []
+    for l in l1:
+        c = l2.count(l)
+        s = l * c
+        print(s)
+        d.append(s)
+    print(sum(d))
+
+def main():
+    l1 = []
+    l2 = []
+    d = []
+    with open("input_1.txt", 'r') as f:
+        items = f.readlines()
+        for i in items:
+            locations = i.strip('\n').split('   ')
+            l1.append(int(locations[0]))
+            l2.append(int(locations[1]))
+
+    do_the_thing(l1, l2)
+
+
+main()
