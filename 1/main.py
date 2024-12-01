d256a26d7d92117b7b1ba56f0697a1ced42aed6d
lemonprogis <edward.briggler@gmail.com>
2024-12-01 14:47:20 -0600

    day 1

---
+++ 1/main.py
@@ -0,0 +1,29 @@
+
+
+def do_the_thing(l1, l2):
+    d = []
+    for i, l in enumerate(l1):
+        x = l 
+        y = l2[i]
+        s = abs(x - y)
+        d.append(s)
+    print(sum(d))
+
+def main():
+    l1 = []
+    l2 = []
+    d = []
+    with open("input.txt", 'r') as f:
+        items = f.readlines()
+        for i in items:
+            locations = i.strip('\n').split('   ')
+            l1.append(int(locations[0]))
+            l2.append(int(locations[1]))
+    
+    l1.sort()
+    l2.sort()
+
+    do_the_thing(l1, l2)
+
+
+main()
